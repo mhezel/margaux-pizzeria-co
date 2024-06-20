f0eb6a73c632359defeed9560219ff3a9bd90f44
Mhezel Mohammad <mhezelkhan@gmail.com>
2024-06-20 16:02:04 +0800

    Updates: Create Order using Cart
    Updates: Create Order using Cart

--- margaux's-pizzeria-co/src/features/order/CreateOrder.jsx
+++ margaux's-pizzeria-co/src/features/order/CreateOrder.jsx
@@ -1,8 +1,14 @@
 // import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
-import Button from "../../ui/Button";
 import { useSelector } from "react-redux";
+import { clearItemsToCart, getCart, getTotalAmountPriceOfPizzas } from "../cart/cartSlice";
+import { getUsername } from "../user/userSlice";
+import { formatCurrency } from "../../utils/helpers";
+import { useState } from "react";
+import Button from "../../ui/Button";
+import EmptyCart from "../cart/EmptyCart";
+import store from "../../store";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -10,37 +16,46 @@ const isValidPhone = (str) =>
     str,
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+// static
+// const fakeCart = [
+//   {
+//     pizzaId: 12,
+//     name: "Mediterranean",
+//     quantity: 2,
+//     unitPrice: 16,
+//     totalPrice: 32,
+//   },
+//   {
+//     pizzaId: 6,
+//     name: "Vegetale",
+//     quantity: 1,
+//     unitPrice: 13,
+//     totalPrice: 13,
+//   },
+//   {
+//     pizzaId: 11,
+//     name: "Spinach and Mushroom",
+//     quantity: 1,
+//     unitPrice: 15,
+//     totalPrice: 15,
+//   },
+// ];
 
 function CreateOrder() {
+
+  const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
   const formErrors = useActionData();
-  const userName = useSelector(state => state.user.userName);
   const isSubmitting = navigation.state === "submitting";
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const userName = useSelector(getUsername);
+  const cart = useSelector(getCart);
+
+
+  const totalCartAmountOrder = useSelector(getTotalAmountPriceOfPizzas);
+  const priorityPrice = withPriority ? totalCartAmountOrder * 0.2 : 0;
+  const totalPrice = totalCartAmountOrder + priorityPrice;
+
+  if(!cart.length) return <EmptyCart/>
 
   return (
     <div className="px-4 py-6">
@@ -73,8 +88,8 @@ function CreateOrder() {
             name="priority"
             id="priority"
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority" className="font-medium">Want to yo give your order priority?</label>
         </div>
@@ -82,7 +97,7 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button disabled={isSubmitting} type="primary">
-            {isSubmitting ? "Submitting order" : "Order now"}
+            {isSubmitting ? "Submitting order" : `Order now for ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
       </Form>
@@ -97,7 +112,7 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === "true",
   };
 
   //error-handling & validation
@@ -107,6 +122,7 @@ export async function action({ request }) {
   if (Object.keys(errors).length > 0) return errors;
 
   const newOrder = await createOrder(order);
+  store.dispatch(clearItemsToCart()); // hacks to clear cart-overview after ordering
   return redirect(`/order/${newOrder.id}`);
 }
 
--- margaux's-pizzeria-co/src/features/order/Order.jsx
+++ margaux's-pizzeria-co/src/features/order/Order.jsx
@@ -42,7 +42,7 @@ function Order() {
       </div>
 
       <ul className="divide-y divide-stone-200 border-b border-t">
-        {cart.map(item => ( <OrderItem item={item} key={item.id}/>))}
+        {cart.map(item => ( <OrderItem item={item} key={item.pizzaId}/>))}
       </ul>
      
       <div className="space-y-2 bg-stone-200 py-5 px-6">
@@ -56,7 +56,6 @@ function Order() {
 
 export async function loader({ params }) {
   const order = await getOrder(params.orderId);
-
   return order;
 }
 
